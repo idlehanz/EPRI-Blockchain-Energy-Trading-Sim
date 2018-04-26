28b00b3222fb2a12920c6fd44c877930bd5095fb
David Trentham <dtrentha@vols.utk.edu>
2018-04-26 02:38:15 +0000

    ready for demo

--- voltchain/truffle.js
+++ voltchain/truffle.js
@@ -2,10 +2,10 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-    development: {
+    mytestnet: {
       //host: "127.0.0.1",
-      host: "192.168.0.6", 
-      port: 7545,
+      host: "127.0.0.1", 
+      port: 8545,
       network_id: "*", // Match any network id
       gas: 4600000
     }
